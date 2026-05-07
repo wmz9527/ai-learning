edd7ac7e1f0a3a418138ae3c37a63c2f14eac575
wmz <897472291@qq.com>
2026-05-07 15:08:30 +0800

    init

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(9527, () => console.log('Server running on http://localhost:9527'));
